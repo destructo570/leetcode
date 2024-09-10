0fbb3f9080eb4bc99804dc17c3b067225cc793d9
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-09-10 15:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (98.64%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int[] n:matrix){
+            boolean is = binarySearch(n, target);
+            if(is){
+                return is; 
+            }
+        }
+        return false;
+    }
+    
+    public boolean searchMatrixOptimal(int[][] matrix, int target) {
+        int m=0; int n=matrix[0].length-1;
+        
+        while(m<matrix.length && n>=0){
+            if(matrix[m][n]==target)
+                return true;
+            else if(matrix[m][n]>target)
+                n--;
+            else 
+                m++;   
+        }
+        return false;
+    }
+    
+    public boolean binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start<=end){
+            int mid = (start+end)/2;
+            int item = arr[mid];
+            
+            if(item==target){
+                return true;
+            }else if(item<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        
+        return false;
+    }
+}
