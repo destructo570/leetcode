c779cbe40c2eea56d98d3e04ac248f4deb58c325
Vishal Kashi <dev.vishalkashi@gmail.com>
2025-01-12 16:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (57.64%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+
+    // public void sortByMergeSort(int[] nums, int lm int h){
+    //     if(l<h){
+    //         int mid = (l+h)/2;
+    //         sortByMergeSort(nums, mid, h);
+    //         sortByMergeSort(nums, l, mid-1);
+    //         mergeArrays(nums, mid, l, h);
+    //     }
+    // }
+
+    // public void mergeArrays(){
+        
+    // }
+}
