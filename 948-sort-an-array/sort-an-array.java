538a6e90752011fe2fefa5fd5dc95f8de4036be4
Vishal Kashi <dev.vishalkashi@gmail.com>
2025-01-21 18:51:32 +0530

    Time: 70 ms (19.96%) | Memory: 55.6 MB (79.46%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -2,37 +2,40 @@ class Solution {
     public int[] sortArray(int[] nums) {
         mergeSort(nums, 0, nums.length-1);
         return nums;
-   }
+    }
 
-   public void mergeSort(int arr[], int l, int r) {
-        if(l<r){
-            int mid = (l+r)/2;
-            
-            mergeSort(arr, l, mid);
-            mergeSort(arr, mid+1, r);
-            merge(arr, mid, l, r);
+    public void mergeSort(int[] nums, int l, int h){
+        if(l<h){
+            int mid = (l+h)/2;
+            mergeSort(nums, l, mid);
+            mergeSort(nums, mid+1, h);
+            merge(nums, mid, l, h);
         }
     }
 
-    public void merge(int[] arr, int mid, int l, int h){
-        int l1 = l;
+    public void merge(int[] nums, int mid, int low, int high){
+        int l1 = low;
         int l2 = mid+1;
-        ArrayList<Integer> temp = new ArrayList<>();
-        while(l1 <= mid && l2 <= h){
-            if(arr[l1] <= arr[l2] ) {
-                temp.add(arr[l1++]);
-            }else {
-                temp.add(arr[l2++]);
-            };
-        }
-        
-        
-        while(l1 <= mid) temp.add(arr[l1++]);
-        while(l2 <= h) temp.add(arr[l2++]);
-        
-        for(int i=l;i<=h;i++){
-            arr[i] = temp.get(i-l);
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        while(l1 <= mid && l2 <= high){
+            if(nums[l1] <= nums[l2]){
+                result.add(nums[l1++]);
+            }else{
+                result.add(nums[l2++]);
+            }
         }
 
+        while(l1<=mid){
+            result.add(nums[l1++]);
+        }
+        while(l2<=high){
+            result.add(nums[l2++]);
+        }
+        
+        for(int i=low; i<=high; i++){
+            nums[i] = result.get(i-low);
+        }
     }
 }
