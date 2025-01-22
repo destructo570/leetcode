fd09813251ed1695d28b3f1d63f84cdf286bba02
Vishal Kashi <dev.vishalkashi@gmail.com>
2025-01-22 21:31:14 +0530

    Time: 1 ms (73.93%) | Memory: 45.6 MB (64.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,5 +1,48 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        setZeroesOpt2(matrix);
+    }
+    public void setZeroesOpt2(int[][] matrix) {
+
+        boolean firstRow=false;
+        boolean firstCol=false;
+        for(int i=0; i<matrix.length; i++){ 
+            if(matrix[i][0]==0) firstCol=true;
+        }
+        for(int i=0; i<matrix[0].length; i++){
+            if(matrix[0][i]==0) firstRow=true;
+        }
+
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }   
+        }
+
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j]=0;
+                }
+            }   
+        }
+
+        if(firstCol){
+            for(int i=0; i<matrix.length; i++){ 
+                matrix[i][0]=0;
+            }
+        }
+        if(firstRow){
+            for(int i=0; i<matrix[0].length; i++){
+                matrix[0][i]=0;
+            }
+        }
+
+    }
+    public void setZeroesOpt1(int[][] matrix) {
         HashSet<Integer> hsr = new HashSet<>();
         HashSet<Integer> hsc = new HashSet<>();
 
