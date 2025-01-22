38c4564d548c932c7cd927cc502fdf5a01ad05e3
Vishal Kashi <dev.vishalkashi@gmail.com>
2025-01-22 17:13:00 +0530

    Time: 3 ms (100.00%) | Memory: 76.9 MB (74.13%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,6 +1,22 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        return naive(nums);
+        return optimal(nums);
+    }
+
+    public int[] optimal(int[] nums){
+        int[] ans = new int[nums.length];
+        int pos=0; 
+        int neg=1;
+        for(int num:nums){
+            if(num>0){
+                ans[pos]=num;
+                pos+=2;
+            }else{
+                ans[neg]=num;
+                neg+=2;
+            }
+        }
+        return ans;
     }
 
     public int[] naive(int[] nums) {
