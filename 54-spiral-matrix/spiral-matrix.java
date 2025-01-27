d23d27bb19b9adf1e1b926e15a523a98efaa42f8
Vishal Kashi <dev.vishalkashi@gmail.com>
2025-01-27 18:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (42.30%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -5,31 +5,36 @@ class Solution {
         int finalOut = matrix.length*matrix[0].length;
         List<Integer> ans = new ArrayList<>();
         int TL=0, TR=col, BL=0, BR=row;
-        while(ans.size() != finalOut){
+        while(TL<=BR && BL<=TR){
             //Print TL to TR
             for(int i=TL; i<=TR; i++){
-                if(ans.size() == finalOut) return ans;
+                // if(ans.size() == finalOut) return ans;
                 ans.add(matrix[TL][i]);
             }
             TL++;
             //Print TL to BL
             for(int i=TL; i<=BR; i++){
-                if(ans.size() == finalOut) return ans;
+                // if(ans.size() == finalOut) return ans;
                 ans.add(matrix[i][TR]);
             }
             TR--;
             //Print BL to BR
+            if(TL<=BR){
+
             for(int i=TR; i>=BL; i--){
-                if(ans.size() == finalOut) return ans;
+                // if(ans.size() == finalOut) return ans;
                 ans.add(matrix[BR][i]);
             }
             BR--;
-            //Print BL to TL
+            }
+            if(BL<=TR){
+                //Print BL to TL
             for(int i=BR; i>=TL; i--){
-                if(ans.size() == finalOut) return ans;
+                // if(ans.size() == finalOut) return ans;
                 ans.add(matrix[i][BL]);
             }
             BL++;
+            }
         }
        return ans;
     }
